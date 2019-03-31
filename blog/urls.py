fd7c0432f1d3000f65bd9ffa383801e3354cee30
yuliyabelyak <yul.belyak@gmail.com>
2019-03-31 17:02:11 +0600

    final

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-   		 path('', views.post_list, name='post_list'),
+   		 path('', views.homework, name='homework'),
+         path('register', views.register, name='register'),
+   		 path('gallery', views.gallery, name='gallery'),
+   		 path('news', views.news, name='news'),
+   		 path('merch', views.merch, name='merch'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
+
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/post_list.html',  {'posts': posts})
+
+def homework(request):
+    return render(request, 'blog/homework.html')
+
+def register(request):
+    return render(request, 'blog/register.html')
+
+def merch(request):
+    return render(request, 'blog/merch.html')
+
+def news(request):
+    posts = Post.objects.all()
+
+    return render(request, 'blog/news.html',  {'posts': posts})
+
+def gallery(request):
+    return render(request, 'blog/gallery.html')
+
 
 # Create your views here.
